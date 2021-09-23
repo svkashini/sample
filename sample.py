d00ec4f2dcf130b547dbc6c97d7030d585972951
svkashini <87290825+svkashini@users.noreply.github.com>
2021-09-23 22:19:54 +0900

    Add files via upload

--- sample.py
+++ sample.py
@@ -5,15 +5,21 @@ st.title('アップされたcsvファイルを３倍にするアプリ')
 
 upload_file = st.file_uploader('csvファイルをアップロードしてください。')
 
-if st.button('３倍になったファイルをダウンロード'):
-    if upload_file is not None:
-        df = pd.read_csv(upload_file)
-        temp = pd.DataFrame()
-        i = 0 
-        while i < 3:
-            temp = pd.concat([temp, df], axis=0)
-            i += 1
-        temp.to_csv('download.csv')
+if upload_file is not None:
+    df = pd.read_csv(upload_file)
+    temp = pd.DataFrame([])
+    i = 0 
+    while i < 3:
+        temp = pd.concat([temp, df], axis=0)
+        i += 1
+
+    st.download_button(
+        label='３倍になったファイルをダウンロード',
+        data=temp.to_csv(),
+        file_name='download.csv',
+        mime='text/csv',
+    )
+
 
 
 
